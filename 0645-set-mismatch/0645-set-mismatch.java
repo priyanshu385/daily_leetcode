d617f062ffb8abdd5e551f334cf278fa633004e9
Priyanshu Kushwaha <126158607+priyanshu385@users.noreply.github.com>
2024-08-23 11:01:06 +0530

    Time: 17 ms (10.83%), Space: 46 MB (6.52%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+       Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        int duplicate = -1;
+        int missing = -1;
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int i = 1; i <= n; i++) {
+            if (map.containsKey(i)) {
+                if (map.get(i) == 2) {
+                    duplicate = i;
+                }
+            } else {
+                missing = i;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
