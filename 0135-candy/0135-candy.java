489cc2535244329dc2289179745e355841f08e20
Priyanshu Kushwaha <126158607+priyanshu385@users.noreply.github.com>
2024-12-05 12:14:05 +0530

    Time: 6 ms (15.87%), Space: 45.2 MB (90.88%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int candy(int[] ratings) {
+       int[] ans = new int[ratings.length];
+       Arrays.fill(ans, 1);
+       for(int i = 1; i < ratings.length; i++){
+        if(ratings[i] > ratings[i - 1]){
+            ans[i] = ans[i - 1] + 1;
+        }
+       }
+       for(int j = ratings.length - 2; j >= 0; j--){
+        if(ratings[j] > ratings[j + 1]){
+            ans[j] = Math.max(ans[j], ans[j + 1] + 1);
+
+        }
+       }
+       return Arrays.stream(ans).sum();
+    }
+}
